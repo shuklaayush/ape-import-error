79b07c3c2f3800972938edb0b3200a28df5b084c
Ayush Shukla <shuklaayush247@gmail.com>
2022-10-08 18:01:07 +0400

    feat: add mre

---
+++ contracts/B.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {A} from
+    "././././././././././././././././././././././././././././././././././././././././././././././././././A.sol";
+
+contract B is A {
+    uint256 b;
+}
